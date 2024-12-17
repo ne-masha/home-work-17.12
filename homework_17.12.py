fc9777fb1026eb1e3d3ab2f641174ddf6b070725
ne-masha <mnevskaa056@gmail.com>
2024-12-17 14:10:51 +0300

    Add files via upload

---
+++ homework_17.12.py
@@ -0,0 +1,6 @@
+number_of_homework = 12
+spent_hours = 1.5
+course_name = "Python"
+time_per_task = spent_hours / number_of_homework
+
+print("Курс: " + course_name, ". Всего задач: ", number_of_homework, ". Затрачено часов: ", spent_hours, ". Среднее время выполнения: ", time_per_task, "часа.")
